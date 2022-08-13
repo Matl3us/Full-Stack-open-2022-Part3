b1ed8a61c86b36ca1ad28d981f65222fe5663861
Mateusz Szczęch <matle0410@gmail.com>
2022-08-13 17:26:23 +0200

    Phonebook backend step3

--- index.js
+++ index.js
@@ -28,12 +28,23 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
 app.get('/info', (request, response) => {
-    response.send('<p>Phonebook has info for ' 
-    + persons.length + ' people</p><p>'+ new Date() + '</p>')
+    response.send('<p>Phonebook has info for '
+        + persons.length + ' people</p><p>' + new Date() + '</p>')
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
