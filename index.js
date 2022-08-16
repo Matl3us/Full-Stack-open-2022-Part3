63b3b588b0caddb0d75a8a3fec384ee71b845ddd
Mateusz Szczęch <matle0410@gmail.com>
2022-08-16 16:17:50 +0200

    Phonebook full stack

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body', (request) => {
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
